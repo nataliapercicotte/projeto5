fa9aeb0af54a57c6e8e913632c1d4790f44bbe23
percicotte <nmpercicotte@gmail.com>
2026-02-05 12:26:49 -0300

    configurando streamlit para deploy

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-st.title("Meu primeiro app")
-st.write("Projeto Sprint 5")
+df = pd.read_csv('vehicles_us.csv')
+
+st.header('Dashboard de anúncios de carros')
+
+hist_button = st.button('Criar histograma')
+
+if hist_button:
+    st.write('Criando um histograma do preço dos carros')
+    fig = px.histogram(df, x='price')
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Criar gráfico de dispersão')
+
+if scatter_button:
+    st.write('Relação entre preço e quilometragem')
+    fig = px.scatter(df, x='odometer', y='price')
+    st.plotly_chart(fig, use_container_width=True)
